15d0a38aa6be256f5a01beb45034e5b2e8ada616
cuihao <cuihao@shiqiao.com>
2021-05-26 19:08:49 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Groups []Groups `yaml:"groups"`
+}
+
+type Groups struct {
+	Name  string  `yaml:"name"`
+	Rules []Rules `yaml:"rules"`
+}
+
+type Rules struct {
+	Alert       string      `yaml:"alert"`
+	Expr        string      `yaml:"expr"`
+	For         string      `yaml:"for"`
+	Labels      Labels      `yaml:"labels"`
+	Annotations Annotations `yaml:"annotations"`
+}
+
+type Labels struct {
+	Severity string `yaml:"severity"`
+}
+
+type Annotations struct {
+	Summary     string `yaml:"summary"`
+	Description string `yaml:"description"`
+}
+
+//read yaml config
+//注：path为yaml或yml文件的路径
+func ReadYamlConfig(path string) (*Config, error) {
+	conf := &Config{}
+	if f, err := os.Open(path); err != nil {
+		return nil, err
+	} else {
+		yaml.NewDecoder(f).Decode(conf)
+	}
+	return conf, nil
+}
+
+//test yaml
+func main() {
+	conf, err := ReadYamlConfig("ssl_expire.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	byts, err := json.Marshal(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(byts))
+}
